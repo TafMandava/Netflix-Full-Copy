59d0eebed64fa9ee44d5daef76ea2de7ac9a267a
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-11 01:54:02 +0200

    Adding one source of truth

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
-    <div className="App">
       <JumbotronContainer />
-    </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+/*
+    Adding one source of truth. We add every component that we create to this file i.e. the Jumbotron, Card and Footer
+*/
+export { default as Jumbotron } from './jumbotron';
