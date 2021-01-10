1675b4ca2c1649da6759e55ef582ba6569dcab48
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-10 07:33:06 +0200

    Creating JumbotronContainer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import Jumbotron from './components/jumbotron';
-import jumbotronData from './fixtures/jumbotrons';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
     <div className="App">
-      {/* 
-          Encapsulate all three Jumbotrons
-          Loop through all of them
-      */}
-      <Jumbotron.Container>
-        {
-          jumbotronData.map((item) => (
-            <Jumbotron key={item.id} direction={item.direction}>
-              <Jumbotron.Pane>
-                <Jumbotron.Title>{item.title}</Jumbotron.Title>
-                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-              </Jumbotron.Pane>
-              <Jumbotron.Pane>
-                <Jumbotron.Image src={item.image} alt={item.alt} />      
-              </Jumbotron.Pane>
-            </Jumbotron>
-         ))
-        }
-      </Jumbotron.Container>
+      <JumbotronContainer />
     </div>
   );
 }
---
+++ src/containers/jumbotron.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Jumbotron from '../components/jumbotron';
+import jumbotronData from '../fixtures/jumbotrons';
+
+export function JumbotronContainer() {
+  return (
+      /* 
+          Encapsulate all three Jumbotrons
+          Loop through all of them
+      */
+      <Jumbotron.Container>
+        {
+          jumbotronData.map((item) => (
+            <Jumbotron key={item.id} direction={item.direction}>
+              <Jumbotron.Pane>
+                <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+              </Jumbotron.Pane>
+              <Jumbotron.Pane>
+                <Jumbotron.Image src={item.image} alt={item.alt} />      
+              </Jumbotron.Pane>
+            </Jumbotron>
+         ))
+        }
+      </Jumbotron.Container>
+  );
+}
