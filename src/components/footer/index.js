6c96e5b86205ccdbf1e4558211209f4a722b5cea
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-12 03:02:19 +0200

    Creating Footer elements and styling rules

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Inner, Container } from './styles/footer';
+import { Container, Title, Text, Link, Row, Column, Break } from './styles/footer';
 
 export default function Footer({ children, ...restProps }) {
     return (
-       <Inner>
+       <Title>
            { children }
-       </Inner>
+       </Title>
     );
 }
 
@@ -16,3 +16,52 @@ Footer.Container = function FooterContainer({ children, ...restProps }) {
         </Container>
     );
 } 
+
+Footer.Row = function FooterRow({ children, ...restProps }) {
+    return (
+        <Row { ...restProps }>
+            { children }
+        </Row>
+    );
+}
+
+Footer.Column = function FooterColumn({ children, ...restProps }) {
+    return (
+        <Column { ...restProps }>
+            { children }
+        </Column>
+    );
+}
+
+Footer.Title = function FooterTitle({ children, ...restProps }) {
+    return (
+        <Title { ...restProps }>
+            { children }
+        </Title>
+    );
+}
+
+Footer.Text = function FooterText({ children, ...restProps }) {
+    return (
+        <Text { ...restProps }>
+            { children }
+        </Text>
+    );
+}
+
+Footer.Link = function FooterLink({ children, ...restProps }) {
+    return (
+        <Link { ...restProps }>
+            { children }
+        </Link>
+    );
+}
+
+Footer.Break = function FooterBreak({ children, ...restProps }) {
+    return(
+        <Break { ...restProps }>
+            { children }
+        </Break>
+    );
+
+}
