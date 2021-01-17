345c226df6895096f7550a09b44c10c7fa85a05a
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-17 19:01:12 +0200

    Adding styling rules for button link

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -18,6 +18,21 @@ export const Logo = styled.img`
 `;
 
 export const ButtonLink = styled(ReactRouterLink)`
+    display: block;
+    background-color: #e50914;
+    width: 84px;
+    height: fit-content;
+    color: white;
+    border: 0;
+    font-size: 15px;
+    border-radius: 3px;
+    padding: 8px 17px;
+    cursor: pointer;
+    text-decoration: none;
+    box-sizing: border-box;
 
+    &:hover {
+        background-color: #f40612;
+    }
 `;
 
