b5319fa87cbd9031dc45f018db56325ecaf36c5a
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-23 19:07:14 +0200

    Connecting our application to Firebase abd Seeding Data

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
 
+/*
+   Connecting our application to Firebase abd Seeding Data
+
+    seed.js
+        -- export function seedDatabase(firebase)
+
+    firebase.prod.js
+        -- import { seedDatabase } from '../seed';
+        -- seedDatabase(firebase);
+
+    index.js
+        -- import { firebase } from './lib/firebase.prod';
+
+    NB: Comment out the statement "seedDatabase(firebase);" after seeding data and refrshing the webpage
+    
+*/
+import { firebase } from './lib/firebase.prod';
 
 render(
     /*
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,18 +1,27 @@
-/* Import firebase firebase app */
+/* Import firebase app */
 import Firebase from 'firebase/app';
 /* We are going to be usking firestore */
 import 'firebase/firestore';
 /* We need firebase auth for authorisation */
 import 'firebase/auth';
-
-/* We need to seed the database */
-
+/* Import seed file once because we do not want to create duplicate data */
+import { seedDatabase } from '../seed';
 
 /* We need a config  */
-const config = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyAX8qMGvBdcrFxRTz8e21Tpof_HJGA5vWY",
+    authDomain: "netflix-full-copy.firebaseapp.com",
+    projectId: "netflix-full-copy",
+    storageBucket: "netflix-full-copy.appspot.com",
+    messagingSenderId: "641671493745",
+    appId: "1:641671493745:web:c154498cb6bea1db0e43d0"
+  };
 
 /* Initialize the App  */
-const firebase = Firebase.initializeApp(config);
+const firebase = Firebase.initializeApp(firebaseConfig);
+
+/* We need to seed the database */
+/* seedDatabase(firebase); */
 
 /* Export firebase because we want to use it at the top of our application  */
 export { firebase };
