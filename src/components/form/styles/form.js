0b92769c0e6217bd67faac8c9f3c6b5386f0cac9
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-24 23:19:15 +0200

    Styling form Container

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,8 +1,26 @@
 import styled from 'styled-components/macro';
 import { Link as ReachRouterLink } from 'react-router-dom';
 
-export const Container = styled.div`
+/*  
+    Put elements in the middle using the following css rules
+        -- margin: auto;
+        -- max-width: 450px;
+    Move Form elements in the middle
+        -- padding: 60px 68px 40px;
+*/
 
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 660px;
+    background-color: rgba(0, 0, 0, 0.75);
+    border-radius: 5px;
+    box-sizing: border-box;
+    width: 100%;
+    margin: auto;
+    max-width: 450px;
+    padding: 60px 68px 40px;
+    margin-bottom: 100px;
 `;
 
 export const Base = styled.form`
